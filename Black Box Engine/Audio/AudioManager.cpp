bdea5a6da668a0d9a33210637bc2d69b34536292
gman20031 <gavin.brandenburger1@gmail.com>
2025-09-15 20:48:59 -0700

    emptied audio

--- Black Box Engine/Audio/AudioManager.cpp
+++ Black Box Engine/Audio/AudioManager.cpp
@@ -1,12 +1,13 @@
 #include "AudioManager.h"
 
-//#include <SDL3/SDL_audio.h>
-//#include <SDL3/SDL_mixer.h>
+#include <SDL3/SDL_audio.h>
 
 namespace BlackBoxEngine
 {
     AudioManager::AudioManager()
     {
+        //SDL_AudioSpec audioSpecs;
+        //audioSpecs.format = SDL_AUDIO_F32
     }
 
     void AudioManager::SetMusicTrack()
