31b5827b25e2e294dc5d6a7f5fb1f673fc7273a1
Lillemor18 <lillemor1834@gmail.com>
2025-09-23 22:40:28 +0200

    added

--- Black Box Game/PlayerMovementComponent.h
+++ Black Box Game/PlayerMovementComponent.h
@@ -3,7 +3,7 @@
 #include <Actors/EngineComponents/MoverComponent.h>
 #include <Actors/EngineComponents/TransformComponent.h>
 #include <Input/InputManager.h>
-
+#include "TileSystem/TileMapComponent.h"
 #include "../Black Box Engine/Math/FVector2.h" // why are we using "../" here and not <>
 
 class PlayerMovementComponent : public BlackBoxEngine::Component
@@ -12,6 +12,11 @@ class PlayerMovementComponent : public BlackBoxEngine::Component
 
     BlackBoxEngine::MoverComponent* m_pMover = nullptr;
     BlackBoxEngine::TransformComponent* m_pTransform = nullptr;
+
+    int m_tileMapId = 0;
+    TileMapComponent* m_pTileMap = nullptr;
+
+    uint64_t m_callbackCodes[8] = {};
 
     std::vector<uint64_t> m_keyDownCodes;
     std::vector<uint64_t> m_keyUpCodes;
